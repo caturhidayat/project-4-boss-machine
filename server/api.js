7b91919f5defa69e0efe0dbc11c1129c0db7619a
caturhidayat <caturhidayat00@gmail.com>
2022-05-11 15:42:04 +0700

    CRUD Minions

--- server/api.js
+++ server/api.js
@@ -1,6 +1,29 @@
 const express = require('express');
 const apiRouter = express.Router();
-
+const db = require('./db')
 
 
 module.exports = apiRouter;
+// MINION ROUTE
+// Get All Minions
+apiRouter.get('/minions', (req, res, next) => {
+    const allMinions = db.getAllFromDatabase('minions')
+    res.send(allMinions)
+})
+// Get Minion by id
+apiRouter.get('/minions/:minionid', (req, res, next) => {
+    const minionid = req.params.minionid
+    const minions = db.getFromDatabaseById('minions', minionid)
+    res.send(minions)
+})
+// Create Minion
+apiRouter.post('/minions', (req, res, next) => {
+    const addMinions = db.addToDatabase('minions', req.body)
+    res.send(addMinions)
+})
+// Edit Minion
+apiRouter.put('/minions/:minionid', (req, res, next) => {
+    const minionid = req.body
+    const editMinions = db.updateInstanceInDatabase('minions', minionid)
+    res.send(editMinions)
+})
